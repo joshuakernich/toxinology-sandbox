eabad6acab7a6fa458409eb2b9a6d0326c44f9c0
Joshua Kernich <joshua.kernich@gmail.com>
2023-02-05 12:35:28 +1100

    Populate Result with real name/binomial

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,18 @@
 import { h } from 'preact';
 import style from './shared.css';
 
-const Result = ({onClick}) => (
-  <div onClick={onClick} class={style.result}>
+const Result = ({current,onClick}) => {
+
+  if(!current) current = {common_names:'Result Name'}
+
+  const name = current.common_names.split(',')[0];
+  const binomial = current.genus + ' ' +current.species;
+
+  return <div onClick={onClick} class={style.result}>
     <img width='200' height='100'/>
-    <h2>Languishing Snake</h2>
-    <h3>Languishio Snakilius</h3>
+    <h2>{name}</h2>
+    <h3>{binomial}</h3>
   </div>
-);
+};
 
 export default Result;
-
